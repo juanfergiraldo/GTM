d52d92a38e8fb5839320a6a2865a9e14907d93ce
Santiago Cadavid <santiagocadavidbte@gmail.com>
2017-04-29 15:51:20 -0500

    Montaje moongose

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendfile("views/index.html");
 });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/gtm');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+   console.log("we're connected!")});
